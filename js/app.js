c72e7c46e50e5f3bb525554ed186876e612f1b48
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-07-01 21:45:25 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -173,7 +173,7 @@ class UI {
 
                 // Añade un botón de editar...
                 const btnEditar = document.createElement('button');
-                btnEditar.onclick = () => cargarEdicion(cursor.value);
+                btnEditar.onclick = () => confirmacionedt(cursor.value);
 
                 btnEditar.classList.add('btn', 'btn-info');
                 btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
@@ -413,6 +413,22 @@ function confirmaciondtl(id)
         {
              eliminarCita(id); // añade la opción de eliminar
             alert('Cita elimnada Exitosamente');
+        }else
+        {
+alert('Cancelacion Exitosa');
+        }
+}
+
+function confirmacionedt(cursor)
+{
+
+    
+     var pregunta = 'Seguro que desea Editar?';
+        var respuesta= confirm(pregunta);
+        if(respuesta)
+        {
+             cargarEdicion(cursor);  
+            alert('Cita cargada Exitosamente');
         }else
         {
 alert('Cancelacion Exitosa');
