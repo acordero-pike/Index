b805dbc011f669be1ee600d5c796b07068397408
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-07-01 21:41:51 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -165,17 +165,8 @@ class UI {
 
                 // Agregar un botón de eliminar...
                 const btnEliminar = document.createElement('button');
-                btnEliminar.onclick = () => 
-                    var pregunta = 'Seguro que desea Eliminar?';
-        var respuesta= confirm(pregunta);
-        if(respuesta)
-        {
-             eliminarCita(id); // añade la opción de eliminar
-            alert('Cita elimnada Exitosamente');
-        }else
-        {
-alert('Cancelacion Exitosa');
-        }
+                btnEliminar.onclick = () => confirmaciondtl(id);
+                  
                
                 btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
                 btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
@@ -414,6 +405,18 @@ function crearDB() {
         console.log('Database creada y lista');
     }
 }
-
+function confirmaciondtl(id)
+{
+  var pregunta = 'Seguro que desea Eliminar?';
+        var respuesta= confirm(pregunta);
+        if(respuesta)
+        {
+             eliminarCita(id); // añade la opción de eliminar
+            alert('Cita elimnada Exitosamente');
+        }else
+        {
+alert('Cancelacion Exitosa');
+        }
+}
 
 
